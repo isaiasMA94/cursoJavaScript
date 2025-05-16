58d60fdbb0321e74e5644e9c83d031bbdb57cc92
isaiasMA94 <isaiasmunoza@gmail.com>
2025-05-15 20:22:24 -0400

    mis primeros pasos

---
+++ Basic/00-helloWordl.js
@@ -0,0 +1,26 @@
+//CURSO JAVA SCRIPT DESDE CERO
+// //cadenas de texto con diferentes comillas 
+/*
+    comillas dobles
+    comillas simples
+    comillas invertidas
+*/
+console.log("Hola mundo")
+console.log('Hola mundo')
+console.log(`Hola mundo`)
+
+console.log("5")// //string
+console.log(5)// //number 
+console.log(5 + 2) //suma
+console.log(5 - 2) //resta
+console.log(5 * 2) //multiplicacion
+console.log(5 / 2) //division
+console.log(5 % 2) //modulo
+console.log(5 ** 2) //exponencial
+console.log(5 + "2") //concatenacion
+console.log(5 - "2") //resta
+console.log(5 * "2") //multiplicacion
+console.log(5 / "2") //division
+console.log(5 % "2") //modulo
+console.log(5 ** "2") //exponencial
+
---
+++ Basic/01-variables.js
@@ -0,0 +1,19 @@
+//VARIABLES
+//VAR YA NO SE DEBE REALIZAR O UTILIZAR
+var helloWorld = "Hola javaScript"
+console.log(helloWorld) 
+
+var helloWorld = "Hola DENUEVO, javaScript 2"
+console.log(helloWorld) //redeclara la variable
+
+//let
+let helloWorld2 = "Hola javaScript 2"
+console.log(helloWorld2) //no se puede redeclarar la variable
+
+helloWorld2 = "Hola DENUEVO, javaScript 2"
+console.log(helloWorld2) //se puede reasignar el valor de la variable
+
+
+//const
+const helloWorld3 = "Hola javaScript 3"
+console.log(helloWorld3) //no se puede redeclarar la variable
