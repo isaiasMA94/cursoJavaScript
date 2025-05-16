27c3e7058669fb8fe10a0d83b424c47a6e46b319
isaiasMA94 <isaiasmunoza@gmail.com>
2025-05-15 22:18:55 -0400

    ejercicios resueltos

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -117,3 +117,5 @@ console.log(name)*/
 
 
 
+
+
---
+++ Basic/operators.js
@@ -0,0 +1,30 @@
+//operadores
+
+//operadores aritméticos
+
+let a = 5
+let b = 10
+
+console.log(a + b); //suma
+console.log(a - b); //resta
+console.log(a * b); //multiplicacion
+console.log(a / b); //division
+
+console.log(a % b); //modulo
+console.log(a ** b); //exponencial
+
+a++ //incremento
+console.log(a); 
+console.log(a++); //incremento postfijo
+
+b-- //decremento
+console.log(b); 
+console.log(a++); //incremento postfijo
+
+
+//operadores de asignación
+
+let myVariable = 2
+console.log(myVariable) //suma
+myVariable += 2 
+console.log(myVariable) //suma
